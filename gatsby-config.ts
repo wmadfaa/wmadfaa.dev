f62a60e4e35e5e8c36de36674db40653e88eeaf2
Wasim Almadfaa <wm.madfaa@gmail.com>
2023-06-12 13:47:28 +0200

    setup automatic code formatting

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,18 +3,22 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `wmadfaa.dev`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.wmadfaa.dev`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }]
+  plugins: [
+    "gatsby-plugin-postcss",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+  ],
 };
 
 export default config;
